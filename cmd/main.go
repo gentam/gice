fdb0143699463f6e8ca10d0d63835359a5838bec
Genta Mochizawa <g.mochizawa@gmail.com>
2025-06-22 23:11:56 +0900

    read flash

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"slices"
 
@@ -37,7 +38,7 @@ func main() {
 		return
 	}
 
-	// [EB82 Appendix A. Sheet 2 of 5 ("USB to SPI/RS232")]
+	// [EB82 Appendix A. Sheet 2 of 5 (USB to SPI/RS232)]
 	// ADBUS0 | 16 | iCE_SCK
 	// ADBUS1 | 17 | iCE_SI
 	// ADBUS2 | 18 | iCE_SO
@@ -46,17 +47,24 @@ func main() {
 	// ADBUS7 | 24 | iCE_RESET
 
 	cs := ft.D4 // ADBUS4 (GPIOLO → CS)
+
 	jedecID, err := readJEDECID(cs, conn)
 	if err != nil {
-		fmt.Println("failed to read JEDEC ID:", err)
+		fmt.Println("read JEDEC ID failed:", err)
 		return
 	}
 	jedecMicronN25Q032 := []byte{0x20, 0xBA, 0x16}
-	if slices.Equal(jedecID[:3], jedecMicronN25Q032) {
-		fmt.Printf("JEDEC ID matches Micron 32Mbit N25Q032 SPI flash (%X)\n", jedecID[:3])
+	if !slices.Equal(jedecID[:3], jedecMicronN25Q032) {
+		fmt.Printf("JEDEC ID does not match Micron 32Mbit N25Q032 SPI flash (%X)\n", jedecID[:3])
 		return
 	}
-	fmt.Printf("JEDEC ID: %X\n", jedecID)
+
+	data, err := readFlash(conn, cs, 0, 256)
+	if err != nil {
+		fmt.Println("read flash failed", err)
+		return
+	}
+	fmt.Println(hex.Dump(data))
 }
 
 func openFT2232H() *ftdi.FT232H {
@@ -78,11 +86,15 @@ func openFT2232H() *ftdi.FT232H {
 	return nil
 }
 
+// [n25q_32mb_3v_65nm.pdf Table 16: Command Set]
+const (
+	cmdReadID = 0x9F
+	cmdRead   = 0x03
+)
+
 func readJEDECID(cs gpio.PinOut, conn spi.Conn) (id []byte, err error) {
 	buf := make([]byte, 4) // command + 3 bytes ID
-	const cmdReadJDECID = 0x9F
-	buf[0] = cmdReadJDECID
-
+	buf[0] = cmdReadID
 	if err = cs.Out(gpio.Low); err != nil {
 		return
 	}
@@ -94,4 +106,40 @@ func readJEDECID(cs gpio.PinOut, conn spi.Conn) (id []byte, err error) {
 		return
 	}
 	return buf[1:], nil
+}
+
+// readFlash splits the read operation into multiple transactions to avoid
+// exceeding the maximum transaction size.
+func readFlash(conn spi.Conn, cs gpio.PinOut, addr, n int) ([]byte, error) {
+	const (
+		maxTx    = 65536 // [AN_108]
+		cmdBytes = 4     // opRead + 24‑bit address
+		maxData  = maxTx - cmdBytes
+	)
+
+	out := make([]byte, n)
+	off := 0
+	for remaining := n; remaining > 0; {
+		chunk := min(remaining, maxData)
+		buf := make([]byte, cmdBytes+chunk)
+		buf[0] = cmdRead
+		buf[1] = byte(addr >> 16)
+		buf[2] = byte(addr >> 8)
+		buf[3] = byte(addr)
+		// tx[4:] dummy bytes
+
+		cs.Out(gpio.Low)
+		if err := conn.Tx(buf, buf); err != nil {
+			cs.Out(gpio.High)
+			return nil, err
+		}
+		cs.Out(gpio.High)
+
+		copy(out[off:], buf[cmdBytes:])
+
+		addr += chunk
+		off += chunk
+		remaining -= chunk
+	}
+	return out, nil
 }
