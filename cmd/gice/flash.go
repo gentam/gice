92c44bd5794c3ea039523ba721db8c1764a9e328
Genta Mochizawa <g.mochizawa@gmail.com>
2025-08-03 21:23:58 +0900

    separate flash.go

---
+++ cmd/gice/flash.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"time"
+
+	"periph.io/x/conn/v3/gpio"
+	"periph.io/x/conn/v3/spi"
+)
+
+// [n25q_32mb_3v_65nm.pdf|Table 16: Command Set]
+// [W25Q128JV-DTR|8.1.2 Instruction Set Table 1]
+const (
+	flashCmdReleasePowerDown = 0xAB
+	flashCmdReadID           = 0x9F
+	flashCmdRead             = 0x03
+)
+
+var knownFlashIDs = map[[3]byte]string{
+	{0x20, 0xBA, 0x16}: "Micron N25Q032",
+	{0xEF, 0x70, 0x18}: "Winbond W25Q128JVIM",
+}
+
+func isKnownFlashID(id [3]byte) (string, bool) {
+	if name, ok := knownFlashIDs[id]; ok {
+		return name, true
+	}
+	return "", false
+}
+
+func releasePowerDown(conn spi.Conn, cs gpio.PinOut) error {
+	buf := []byte{flashCmdReleasePowerDown, 0, 0, 0, 0}
+	if err := cs.Out(gpio.Low); err != nil {
+		return err
+	}
+	if err := conn.Tx(buf, buf); err != nil {
+		cs.Out(gpio.High)
+		return err
+	}
+	err := cs.Out(gpio.High)
+	time.Sleep(3 * time.Microsecond) // [W25Q128JV-DTR|9.6 AC Electrical Characteristics: tRES1]
+	return err
+}
+
+func readFlashID(conn spi.Conn, cs gpio.PinOut) (id [3]byte, err error) {
+	buf := []byte{flashCmdReadID, 0, 0, 0}
+	if err = cs.Out(gpio.Low); err != nil {
+		return
+	}
+	if err = conn.Tx(buf, buf); err != nil {
+		cs.Out(gpio.High)
+		return
+	}
+	return [3]byte(buf[1:]), cs.Out(gpio.High)
+}
+
+// readFlash splits the read operation into multiple transactions to avoid
+// exceeding the maximum transaction size.
+func readFlash(conn spi.Conn, cs gpio.PinOut, addr, n int) ([]byte, error) {
+	const (
+		maxTx    = 65536 // [AN_108]
+		cmdBytes = 4     // opRead + 24‑bit address
+		maxData  = maxTx - cmdBytes
+	)
+
+	out := make([]byte, n)
+	off := 0
+	for remaining := n; remaining > 0; {
+		chunk := min(remaining, maxData)
+		buf := make([]byte, cmdBytes+chunk)
+		buf[0] = flashCmdRead
+		buf[1] = byte(addr >> 16)
+		buf[2] = byte(addr >> 8)
+		buf[3] = byte(addr)
+		// tx[4:] dummy bytes
+
+		if err := cs.Out(gpio.Low); err != nil {
+			return nil, err
+		}
+		txErr := conn.Tx(buf, buf)
+		if csErr := cs.Out(gpio.High); csErr != nil {
+			return nil, csErr
+		}
+		if txErr != nil {
+			return nil, txErr
+		}
+
+		copy(out[off:], buf[cmdBytes:])
+
+		addr += chunk
+		off += chunk
+		remaining -= chunk
+	}
+	return out, nil
+}
