2021c887d5d2ace4d7ae832bef22db57fdcdfaa3
Genta Mochizawa <genta@mochizawa.org>
2025-08-11 13:17:57 +0900

    separate Flash type

--- cmd/gice/read.go
+++ cmd/gice/read.go
@@ -29,16 +29,16 @@ func readCommand(args []string) {
 	d.ResetFPGA(false) // prevent FPGA from acting as a SPI master
 	defer d.ResetFPGA(true)
 
-	if err := d.FlashPowerUp(); err != nil {
+	if err := d.Flash.PowerUp(); err != nil {
 		fatalf("flash power up failed: %v", err)
 	}
-	defer d.FlashPowerDown()
+	defer d.Flash.PowerDown()
 
-	flashID, err := d.ReadFlashID()
+	flashID, err := d.Flash.ReadID()
 	if err != nil {
 		fatalf("read flash ID failed: %v", err)
 	}
-	name, known := d.IsKnownFlashID(flashID)
+	name, known := d.Flash.IsKnown(flashID)
 	if idOnly {
 		fmt.Printf("%X\t%s\n", flashID, name)
 		return
@@ -47,7 +47,7 @@ func readCommand(args []string) {
 		fmt.Fprintf(os.Stderr, "unknown flash ID (%X)\n", flashID)
 	}
 
-	data, err := d.ReadFlash(0, nread)
+	data, err := d.Flash.Read(0, nread)
 	if err != nil {
 		fatalf("read flash failed: %v", err)
 	}
