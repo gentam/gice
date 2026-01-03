2ac31663b9663ca01de16015d65bdadb4a499c2c
Genta Mochizawa <genta@mochizawa.org>
2026-01-03 13:01:13 +0900

    fix pack output path

--- cmd/gice/pack.go
+++ cmd/gice/pack.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/gentam/gice"
 )
@@ -10,39 +13,41 @@ import (
 func packCommand(args []string) {
 	fs := flag.NewFlagSet("pack", flag.ExitOnError)
 	var (
-		inFilename   string
-		outFilename  string
+		inFilePath   string
+		outFilePath  string
 		skipBRAMInit bool
 	)
-	fs.StringVar(&inFilename, "i", "", "input file (default stdin)")
-	fs.StringVar(&outFilename, "o", "", `output file (default: <input file>.bin; stdin → "out.bin")`)
+	fs.StringVar(&inFilePath, "i", "", "input file (default stdin)")
+	fs.StringVar(&outFilePath, "o", "", `output file (default: <input file>.bin; stdin → "out.bin")`)
 	fs.BoolVar(&skipBRAMInit, "n", false, "skip initializing BRAM")
 	fs.Parse(args)
 
 	inFile := os.Stdin
-	if inFilename != "" {
+	if inFilePath != "" {
 		var err error
-		inFile, err = os.Open(inFilename)
+		inFile, err = os.Open(inFilePath)
 		if err != nil {
-			fatalf("open %q: %v", inFilename, err)
+			fatalf("open %q: %v", inFilePath, err)
 		}
 		defer inFile.Close()
 	}
 
-	if outFilename == "" {
-		if inFilename == "" {
-			outFilename = "out.bin"
+	if outFilePath == "" {
+		if inFilePath == "" {
+			outFilePath = "out.bin"
 		} else {
-			outFilename = inFilename + ".bin"
+			inFile := filepath.Base(inFilePath)
+			outFilePath = strings.TrimSuffix(inFile, ".asc") + ".bin"
 		}
 	}
-	outFile, err := os.Create(outFilename)
+	outFile, err := os.Create(outFilePath)
 	if err != nil {
-		fatalf("create %q: %v", outFilename, err)
+		fatalf("create %q: %v", outFilePath, err)
 	}
 	defer outFile.Close()
 
 	if err := gice.Pack(inFile, outFile); err != nil {
 		fatalf("pack: %v", err)
 	}
+	fmt.Println(outFilePath)
 }
